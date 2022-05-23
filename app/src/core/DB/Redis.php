90ba0a53555d015d44fc7d9c5d596568c9cd2a9c
Alexander Mironov <alexan9610@gmail.com>
2022-05-23 21:11:36 +0300

    Update redis and add docs

--- app/src/core/DB/Redis.php
+++ app/src/core/DB/Redis.php
@@ -2,10 +2,21 @@
 
 namespace App\Anet\DB;
 
+/**
+ * **Redis** -- simple wrapper for Redis PHP library
+ * https://github.com/phpredis/phpredis
+ */
 final class Redis
 {
+    /**
+     * @var null|\Redis $connect `private` static connect to Redis storage
+     */
     private static ?\Redis $connect = null;
 
+    /**
+     * **Method** get instance of connect to redis storage
+     * @return \Redis connect to redis storage
+     */
     public static function getConnect() : \Redis
     {
         if (self::$connect === null) {
@@ -17,6 +28,12 @@ final class Redis
         return self::$connect;
     }
 
+    /**
+     * **Method** set json value to redis storage by key name with adding unique id posfix to key
+     * @param string $key base name of redis key
+     * @param mixed $data saving data
+     * @return string redis key of current value
+     */
     public static function set(string $key, mixed $data) : string
     {
         do {
@@ -28,6 +45,24 @@ final class Redis
         return $currentKey;
     }
 
+    /**
+     * **Method** set serialized PHP object to redis storage
+     * @param string $key name of redis key
+     * @param object $object saving PHP object
+     * @return string
+     */
+    public static function setObject(string $key, object $object) : string
+    {
+        self::getConnect()->set($key, serialize($object));
+
+        return $key;
+    }
+
+    /**
+     * **Method** fetch values with deleting from redis storage
+     * @param string $keys key name, support mask 'name*' whitch return all suitable keys
+     * @return array list of values
+     */
     public static function fetch(string $keys) : array
     {
         $keyList = self::getConnect()->keys($keys);
@@ -41,5 +76,22 @@ final class Redis
         self::getConnect()->del($keyList);
 
         return $result;
+    }
+
+    /**
+     * **Method** fetch php object with deleting from redis storage
+     * @param string $key key of object in storage
+     * @return null|object if object exist - return object, else - null
+     */
+    public static function fetchObject(string $key) : ?object
+    {
+        if (! self::getConnect()->exists($key)) {
+            return null;
+        }
+
+        $object = unserialize(self::getConnect()->get($key));
+        self::getConnect()->del($key);
+
+        return is_object($object) ? $object : null;
     }
 }
