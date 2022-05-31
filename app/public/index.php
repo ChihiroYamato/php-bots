32680847ec3066a1c01cf49e24ad2c4c91c1ef62
Alexander Mironov <alexan9610@gmail.com>
2022-06-01 00:31:07 +0300

    Edit Nginx config

--- app/public/index.php
+++ app/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use App\Anet\Bots;
 use App\Anet\YouTubeHelpers;
@@ -40,7 +40,6 @@ if (! empty($_SESSION['project'])) {
 
     return 0;
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
